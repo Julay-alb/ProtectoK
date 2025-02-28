a802322ae2a396a069df3f715f095fab0e83b6fd
MarlonAngarita <estidangarita21@gmail.com>
2025-02-27 19:49:36 -0500

    router actualizado

--- Appkutsa_BE/routers.py
+++ Appkutsa_BE/routers.py
@@ -2,6 +2,7 @@ from modules.rol_map import rolRouter
 from modules.foro_map import foroRouter
 from modules.categoria_map import categoriaRouter
 from modules.cursos_map import cursosRouter
+from modules.quiz_map import quizRouter
 
 
 def include_routers(app):
@@ -9,3 +10,4 @@ def include_routers(app):
     app.include_router(foroRouter)
     app.include_router(categoriaRouter)
     app.include_router(cursosRouter)
+    app.include_router(quizRouter)
