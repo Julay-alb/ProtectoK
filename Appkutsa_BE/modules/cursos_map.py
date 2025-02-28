1896fe20970f076587124064ad042184b86f2076
MarlonAngarita <estidangarita21@gmail.com>
2025-02-27 21:28:26 -0500

    actualización sentencias

--- Appkutsa_BE/modules/cursos_map.py
+++ Appkutsa_BE/modules/cursos_map.py
@@ -5,10 +5,15 @@ from Clever_MySQL_conn import cleverCursor, mysqlConn
 
 cursosRouter = APIRouter() #Se crea un objeto de tipo APIRouter
 
+cleverCursor.execute("CREATE TABLE IF NOT EXISTS Cursos (id_Cursos INT AUTO_INCREMENT PRIMARY KEY, Nombre_Curso VARCHAR(255), Descripcion_Curso VARCHAR(255), Contenido_Curso VARCHAR(255))")
+
+
 class cursosDB(BaseModel):
     Nombre_Curso: str
     Descripcion_Curso: str
     Contenido_Curso: str
+
+ 
 
 @cursosRouter.get("/kutsadb_Cursos/", status_code=status.HTTP_302_FOUND)
 async def get_users():
