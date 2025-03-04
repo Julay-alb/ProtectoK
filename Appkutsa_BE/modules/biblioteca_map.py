3da6949b1128055b36b10f1c2265732a99b0ef4e
Julay-alb <albarracin790@gmail.com>
2025-03-04 18:37:44 -0500

    actualizaion

--- Appkutsa_BE/modules/biblioteca_map.py
+++ Appkutsa_BE/modules/biblioteca_map.py
@@ -10,14 +10,14 @@ class BibliotecaDB(BaseModel):
     Contenido_Biblioteca: str
     descripcion_Biblioteca: str
 
-@bibliotecaDBRouter.get("/kutsadb_Biblioteca/", status_code=status.HTTP_302_FOUND)
+@BibliotecaDBRouter.get("/kutsadb_Biblioteca/", status_code=status.HTTP_302_FOUND)
 async def get_users():
     selectAll_query = 'SELECT * FROM Biblioteca'
     cleverCursor.execute(selectAll_query)
     result = cleverCursor.fetchall()
     return result
 
-@bibliotecaDBRouter.get("/kutsadb_Biblioteca/{Biblioteca_id}", status_code=status.HTTP_200_OK) #Se crea una ruta para obtener una categoria por su id
+@BibliotecaDBRouter.get("/kutsadb_Biblioteca/{Biblioteca_id}", status_code=status.HTTP_200_OK) #Se crea una ruta para obtener una categoria por su id
 def get_user_by_id(Biblioteca_id: int):
     select_query = "SELECT * FROM Biblioteca WHERE id_Biblioteca = %s"
     cleverCursor.execute(select_query, (Biblioteca_id,))
@@ -28,8 +28,8 @@ def get_user_by_id(Biblioteca_id: int):
         raise HTTPException(status_code=404, detail="Biblioteca no encontrada")
 
 
-@bibliotecaDBRouter.post("/kutsadb_Biblioteca/", status_code=status.HTTP_201_CREATED) #Se crea una ruta para insertar una categoria
-def insert_categoria(categoriaPost: bibliotecaDB):
+@BibliotecaDBRouter.post("/kutsadb_Biblioteca/", status_code=status.HTTP_201_CREATED) #Se crea una ruta para insertar una categoria
+def insert_categoria(categoriaPost: BibliotecaDB):
     insert_query = """
     INSERT INTO Biblioteca (Nombre_Biblioteca, Contenido_Biblioteca, descripcion_Biblioteca)
     VALUES (%s, %s, %s)
