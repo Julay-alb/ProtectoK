e006fc0c56b8f20a47a216155d45b3053d2a0130
MarlonAngarita <estidangarita21@gmail.com>
2025-03-06 19:08:14 -0500

    rutas actualizadas

--- Appkutsa_BE/routers.py
+++ Appkutsa_BE/routers.py
@@ -4,6 +4,8 @@ from modules.categoria_map import categoriaRouter
 from modules.cursos_map import cursosRouter
 from modules.quiz_map import quizRouter
 from modules.biblioteca_map import bibliotecaRouter
+from modules.usuarios_map import usuarioRouter
+from modules.modulos_map import modulosRouter
 
 
 def include_routers(app):
@@ -13,3 +15,5 @@ def include_routers(app):
     app.include_router(cursosRouter)
     app.include_router(quizRouter)
     app.include_router(bibliotecaRouter)
+    app.include_router(usuarioRouter)
+    app.include_router(modulosRouter)
